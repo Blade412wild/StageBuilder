7f150bbd7e0666903e89b0612f1ca56cf031591a
Blade412wild <nathanhoh951@gmail.com>
2024-11-15 20:13:59 +0100

    Added TEST Message Spawner

--- FPT_Test/Assets/01_Scripts/ConsoleManager.cs
+++ FPT_Test/Assets/01_Scripts/ConsoleManager.cs
@@ -1,18 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ConsoleManager : MonoBehaviour
 {
+    [Header("'Messages")]
+    [SerializeField] private bool spawnMessages;
+    [SerializeField] private GameObject messagePrefab;
+    [SerializeField] private List<GameObject> messages;
+
+    [Header("Errors")]
+    [SerializeField] private List<CustomError> customErrors = new List<CustomError>();
+
+    private UIDynamicHeightScaler heightScaler;
+    private VerticalLayoutGroup verticalLayoutGroup;
+
+    private ErrorList errorList;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        verticalLayoutGroup = GetComponent<VerticalLayoutGroup>();
+        heightScaler = GetComponent<UIDynamicHeightScaler>();
+
+        customErrors.Add(new CustomError("hallo"));
+        customErrors.Add(new CustomError("Lach eens :)"));
+        customErrors.Add(new CustomError("hallo"));
+        customErrors.Add(new CustomError("Lach eens :)"));
+        customErrors.Add(new CustomError("hallo"));
+        customErrors.Add(new CustomError("hallo"));
+        customErrors.Add(new CustomError("Lach eens :)"));
+        customErrors.Add(new CustomError("hallo"));
+        customErrors.Add(new CustomError("Lach eens :)"));
+        customErrors.Add(new CustomError("Lach eens :)"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (spawnMessages)
+        {
+            if (customErrors.Count == 0) return;
+
+            foreach (var error in customErrors)
+            {
+                Instantiate(messagePrefab, transform);
+            }
+
+
+            // create container size
+            float messageCount = customErrors.Count;
+            float heightmessage = messagePrefab.GetComponent<RectTransform>().sizeDelta.y;
+            float newHeight = messageCount * (heightmessage + verticalLayoutGroup.spacing);
+            heightScaler.CalculateNewYPos(newHeight);
+
+            spawnMessages = false;
+        }
+    }
+
+
+    private void SpawnMessages()
+    {
+
     }
 }
